2c2658548b2068d72c09abd61b8f4f6ed4ce1838
nickem00 <nickemalm00@gmail.com>
2025-04-05 20:18:59 +0200

    Added function in controller to fetch dish by name.

--- src/controllers/dishesController.js
+++ src/controllers/dishesController.js
@@ -15,3 +15,24 @@ export const getAllDishes = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getDishByName = async (req, res, next) => {
+    try {
+        let { name } = req.params;
+        
+        // make it case-insensitive   
+        name = name.toLowerCase();
+
+        const searchedDish = await Dish.findOne({ name: name})
+
+        if (!searchedDish) {
+            return res.status(404).json({ message: "Dish not found" });
+        }
+
+        res.status(200).json(searchedDish)
+
+    } catch (error) {
+        console.error("Error while fetching dish by name from database:");
+        next(error);
+    }
+}
--- src/routes/dishesRoutes.js
+++ src/routes/dishesRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
-    getAllDishes
+    getAllDishes,
+    getDishByName
 } from '../controllers/dishesController.js';
 
 const router = express.Router();
 
 router.get('/', getAllDishes);
+router.get('/:name', getDishByName)
 
 export default router;
